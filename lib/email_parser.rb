28dfdc2698c2d7a12abec177d31b3bf547af5c5b
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-15 10:55:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,9 @@ class EmailParser
   end
 
   def parse
+  # .split to create array of emails. .reject needed to remove extra blank array elements created by regex used. Need to find better way of doing this.
     email_array = @emails.split(/[","\s]/).reject { |x| x == ""}
+  # iterate through new array to add only unique items to @parsed array
     email_array.each do |email|
       if @parsed.include?(email) == false || @parsed == nil
         @parsed << email
